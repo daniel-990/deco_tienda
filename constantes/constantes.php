64686cee6f569390a531f001a637008216847a38
Daniel Arango <danielarango990@gmail.com>
2022-07-06 09:26:38 -0500

    se modifica el archivo de credenciales
    Se modifica el archivo de credenciales

--- constantes/constantes.php
+++ constantes/constantes.php
@@ -1,20 +1,14 @@
 <?php 
 
-    //conexcion a la tienda de wordpress
-    // define('URLTIENDA','https://decohouse.com.co/');
-    // define('USERID','ck_a54a9c7c296ea5d3bdec3fd17bfb7dd80153de61');
-    // define('APIID','cs_399990b2785c78ab164209c4abffa314278c6bdb');
-
     //conexion tienda de pruebas
-    define('URLTIENDA','https://decohouse.com.co/decohouse-tienda');
-    define('USERID','ck_0222274554d3f5539217faa4a877807ba2653647');
-    define('APIID','cs_beae6913ff402aa716756fe399a42d23af2824ce');
+    define('URLTIENDA','');
+    define('USERID','ck_');
+    define('APIID','cs_');
 
 
     //datos bot
-    //https://api.telegram.org/bot5255586585:AAESeB28OXDl2L4GwhsaqHQkV-tY73KX9YA/getUpdates
-    define('APIBOT','5255586585:AAESeB28OXDl2L4GwhsaqHQkV-tY73KX9YA');
-    //define('IDCHAT',177279577); //-> daniel
-    define('IDCHAT',-768882366);
+    define('APIBOT','');
+    //define('IDCHAT',90); //-> daniel
+    define('IDCHAT',-76);
 
 ?>
